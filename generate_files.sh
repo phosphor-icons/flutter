114d5220d326ebb56e9ed055811f78475dddda25
Rurick Maqueo Poisot <rurick.mpoisot@gmail.com>
2021-02-28 20:20:16 -0600

    🚚 renamed tools directory to tool

--- generate_files.sh
+++ generate_files.sh
@@ -5,9 +5,9 @@ set -e
 echo "Generation started using Phosphor.json file"
 
 echo "Generating package phosphor_icons.dart file"
-dart ./tools/generate_icons.dart ./Phosphor.json
+dart ./tool/generate_icons.dart ./Phosphor.json
 
 echo "Generating example app all_icons.dart file"
-dart ./tools/generate_app.dart ./Phosphor.json
+dart ./tool/generate_app.dart ./Phosphor.json
 
 echo "All files generated"
