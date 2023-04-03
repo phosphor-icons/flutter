c1193f6390afb0121ec0e7d885ad98e92200b718
Rurick Maqueo Poisot <rurick.mpoisot@gmail.com>
2023-04-02 18:29:00 -0600

    add PhosphorIconData implementation for flat and duotone styles

--- lib/src/phosphor_icon_data.dart
+++ lib/src/phosphor_icon_data.dart
@@ -8,5 +8,18 @@ class PhosphorIconData extends IconData {
           codePoint,
           fontFamily: 'Phosphor$style',
           fontPackage: 'phosphor_flutter',
+          matchTextDirection: true,
         );
+}
+
+class PhosphorFlatIconData extends PhosphorIconData {
+  const PhosphorFlatIconData(int codePoint, String style)
+      : super(codePoint, style);
+}
+
+class PhosphorDuotoneIconData extends PhosphorIconData {
+  const PhosphorDuotoneIconData(int codePoint, this.secondary)
+      : super(codePoint, 'duotone');
+
+  final PhosphorIconData secondary;
 }
